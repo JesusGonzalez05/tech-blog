c96a8d832afdc089de190c3fac75544c05ca2a1f
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-06 16:23:22 -0500

    began defining Comment model.

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        // // id thats an integer, cant be null and auto increments
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        // text content
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        // use utils for date if not entered - defaulted value
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        // user id that references the user model object 
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        // post id that references the post model object 
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+    },
+
+);
